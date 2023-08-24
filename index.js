97ddc71bbc4f98bbcb6e4fe140ddc06ecdbbe806
Antony <antonybush95@gmail.com>
2023-08-24 18:56:09 +0530

    Ex 3.8

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+morgan.token('datan',(req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :datan'));
 
 let persons = [
   {
