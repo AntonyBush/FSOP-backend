6830511ac38db20a7aff185d5ccb37b245c95cee
Antony <antonybush95@gmail.com>
2023-08-25 21:10:37 +0530

    Ex 3.13-3.14

--- index.js
+++ index.js
@@ -1,53 +1,34 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 morgan.token("datan", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :datan")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const Person = require("./models/person");
 
 //GET
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((result) => response.json(result));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).send("Nakku");
-  }
+  const id = request.params.id;
+  Person.findById(id)
+    .then((result) => response.json(result))
+    .catch((error) =>
+      response.json({
+        error: "Not found",
+      })
+    );
 });
 
 app.get("/info", (request, response) => {
@@ -61,25 +42,18 @@ app.get("/info", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  const duplicate = persons.filter((person) => person.name === body.name);
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: "Name or Number is missing",
     });
-  } else if (duplicate.length !== 0) {
-    return response.status(409).json({
-      error: "Name must be unique",
-    });
   }
-  const person = {
-    id: Math.floor(Math.random() * 10000),
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-  console.log(person);
-  response.json(person);
+  person.save().then((result) => response.json(result));
 });
 //DELETE
 
